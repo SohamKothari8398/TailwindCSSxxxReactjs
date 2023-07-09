c2d9f93f0ee0a3430b4e79468d74310953e0bfda
SohamKothari8398 <126755623+SohamKothari8398@users.noreply.github.com>
2023-07-09 18:49:59 +0530

    App, index css, js and jsx files
    App, index css, js and jsx files upload

---
+++ App.jsx
@@ -0,0 +1,21 @@
+import Banner from "./components/Banner";
+import Cards from "./components/Cards";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Laptop from "./components/Laptop";
+import Notify from "./components/Notify";
+
+function App() {
+  return (
+    <>
+    <Header/>
+    <Banner/>
+    <Laptop/>
+    <Notify/>
+    <Cards/>
+    <Footer/>
+    </>
+  );
+}
+
+export default App;
